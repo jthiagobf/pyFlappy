f5eba8a8ba4a32bcd841420671de2ce2c215f5f4
unknown <j.thiagobf1992@gmail.com>
2021-11-28 10:11:57 -0300

    envio

---
+++ pyflappy.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import random
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.image.load(os.load.join('imgs', 'pipe.png'))
+IMAGEM_CHAO =
+IMAGEM_BACKGROUND =
+IMAGENS_PASSARO =
