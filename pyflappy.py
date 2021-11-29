2b6da753ecaca2e2ed59b312481b4b9d0ea176da
unknown <j.thiagobf1992@gmail.com>
2021-11-29 12:00:00 -0300

    terceiro commit

--- pyflappy.py
+++ pyflappy.py
@@ -5,7 +5,33 @@ import random
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.image.load(os.load.join('imgs', 'pipe.png'))
-IMAGEM_CHAO =
-IMAGEM_BACKGROUND =
-IMAGENS_PASSARO =
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
+]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+
+class Passaro:
+    IMGS = IMAGENS_PASSARO
+    #ROTAÇÃO DO PASSARO
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Cano:
+    pass
+
+class Chao:
+    pass
